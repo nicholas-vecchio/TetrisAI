e551521b7522d6fe10e0c3814a16058b601c2fe1
Stripesy <nicholas.vecchio02@gmail.com>
2023-10-13 16:54:38 +1100

    Added improved scoring for lines/hard/soft drops.

--- tetris_game.py
+++ tetris_game.py
@@ -16,6 +16,10 @@ SCREEN_HEIGHT = GRID_HEIGHT * CELL_SIZE + PADDING
 WHITE = (255, 255, 255)
 COLORS = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (0, 255, 255), (255, 0, 255), (128, 128, 128)]
 
+# Font
+FONT_SIZE = 30
+font = pygame.font.Font(None, FONT_SIZE)  # Use the default Pygame font
+
 # Create the screen and clock objects
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Tetris")
@@ -43,8 +47,10 @@ def draw_grid_background():
         pygame.draw.line(screen, (200, 200, 200), (x * CELL_SIZE + PADDING, PADDING), (x * CELL_SIZE + PADDING, GRID_HEIGHT * CELL_SIZE + PADDING))
 
 def drop_tetromino(tetromino, x, y):
+    drop_distance = 0
     while is_valid_move(tetromino, x, y + 1):
         y += 1
+        drop_distance += 1
 
     # Find the base tetromino that matches the current one
     matching_tetromino = next((t for t in tetrominoes if tetromino in t), None)
@@ -52,7 +58,7 @@ def drop_tetromino(tetromino, x, y):
     if matching_tetromino is not None:
         place_tetromino_on_grid(tetromino, x, y, tetrominoes.index(matching_tetromino) + 1)
     
-    return GRID_WIDTH // 2, 0
+    return GRID_WIDTH // 2, 0, drop_distance
 
 
 
@@ -68,6 +74,8 @@ def main():
     fall_delay = 500
     fall_timer = pygame.time.get_ticks()
     switched_this_drop = False
+    level = 0
+    lines_cleared_total = 0
 
     while running:
         screen.fill(WHITE)
@@ -102,11 +110,13 @@ def main():
                         tetromino_x, tetromino_y = GRID_WIDTH // 2, 0
                         switched_this_drop = True
                 elif event.key == pygame.K_SPACE:
-                    tetromino_x, tetromino_y = drop_tetromino(current_tetromino[current_rotation], tetromino_x, tetromino_y)
+                    drop_dist = 0
+                    tetromino_x, tetromino_y, drop_dist = drop_tetromino(current_tetromino[current_rotation], tetromino_x, tetromino_y)
                     held_tetromino, current_tetromino = current_tetromino, next_tetromino
                     if not bag:
                         bag = generate_bag()
                     next_tetromino = bag.pop()
+                    score += drop_dist * 2
 
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_DOWN:
@@ -118,6 +128,8 @@ def main():
             fall_timer = current_time
             if is_valid_move(current_tetromino[current_rotation], tetromino_x, tetromino_y + 1):
                 tetromino_y += 1
+                if(fall_delay == 50):
+                    score += 1
             else:
                 place_tetromino_on_grid(current_tetromino[current_rotation], tetromino_x, tetromino_y, tetrominoes.index(current_tetromino) + 1)
                 current_tetromino = next_tetromino
@@ -131,12 +143,30 @@ def main():
                     running = False
 
         lines_cleared = clear_complete_lines()
+        lines_cleared_total += lines_cleared  # Keep track of total lines cleared
+
         # Score logic
-        if lines_cleared:
-            score += lines_cleared * 10
+        if lines_cleared == 1:
+            score += (40 * (level + 1))
+        elif lines_cleared == 2:
+            score += (100 * (level + 1))
+        elif lines_cleared == 3:
+            score += (300 * (level + 1))
+        elif lines_cleared == 4:
+            score += (1200 * (level + 1))
+
+        # Increase level every ten lines
+        #if lines_cleared_total >= 10:
+            #level += 1
+            #lines_cleared_total -= 10  # Reset the counter for the next level
 
         draw_tetromino(current_tetromino[current_rotation], tetromino_x, tetromino_y, COLORS[tetrominoes.index(current_tetromino) % len(COLORS)])
         draw_grid()
+
+        score_text = font.render(f'Score: {score}', True, (0, 0, 0))  # Render the score with black color
+        score_pos = (SCREEN_WIDTH - score_text.get_width() - 10, 10)  # Position to display at top right, 10 pixels from the edge
+        screen.blit(score_text, score_pos)  # Draw the rendered score on the screen
+
         
         pygame.display.flip()
         clock.tick(60)
