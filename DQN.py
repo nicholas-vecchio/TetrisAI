a9d1e7b177c5c341527fef967207261abd5da609
Nicholas Vecchio <88710714+nicholas-vecchio@users.noreply.github.com>
2023-10-19 10:49:09 +1100

    Updated step and learn.

--- DQN.py
+++ DQN.py
@@ -40,37 +40,60 @@ class DQNAgent:
 
     def step(self, state, action, reward, next_state, done):
         action_index = ACTIONS.index(action)
-        self.memory.push(torch.tensor(state["grid"], device=device).view(-1), action_index, torch.tensor([reward], device=device), torch.tensor(next_state["grid"], device=device).view(-1), done)
-        
-        self.total_episode_reward += reward  # Add rewards
-        
+
+        # Store the transition in memory
+        self.memory.push(torch.tensor(state["grid"], device=device).view(-1), 
+                        action_index, 
+                        torch.tensor([reward], device=device), 
+                        None if done else torch.tensor(next_state["grid"], device=device).view(-1), 
+                        done)
+
+        # Add the reward to the episode's total reward
+        self.total_episode_reward += reward
+
+        # If the episode ends, store the total reward and reset
         if done:
             self.rewards_per_episode.append(self.total_episode_reward)
-            self.total_episode_reward = 0  # Reset episode reward
-        
+            self.total_episode_reward = 0
+
+        # Learn from a random batch of transitions if memory is large enough
         if len(self.memory) > self.batch_size:
             transitions = self.memory.sample(self.batch_size)
             self.learn(transitions)
 
+        # Update the target network, copying all weights and biases in DQN
         if len(self.memory) % TARGET_UPDATE == 0:
             self.target_network.load_state_dict(self.qnetwork.state_dict())
 
+
     def learn(self, transitions):
         batch = Transition(*zip(*transitions))
-        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)), device=device, dtype=torch.bool)
-        non_final_next_states = torch.cat([s.clone().detach().float().view(1, -1) for s in batch.next_state if s is not None]).to(device)
+        
+        # Create a mask for non-final (not done) states
+        non_final_next_states_mask = torch.tensor(tuple(map(lambda s: s is not None and not s[4], transitions)), device=device, dtype=torch.bool)
+        
+        non_final_next_states = torch.cat([s.clone().detach().float().view(1, -1) for s in batch.next_state if s is not None and not s[4]]).to(device)
+        
         state_batch = torch.cat([s.clone().detach().float().view(1, -1) for s in batch.state]).to(device)
         action_batch = torch.cat([torch.tensor([a], device=device) for a in batch.action]).to(device)
         reward_batch = torch.cat(batch.reward).to(device)
+        
         state_action_values = self.qnetwork(state_batch).gather(1, action_batch.view(-1, 1))
+        
         next_state_values = torch.zeros(self.batch_size, device=device)
-        next_state_values[non_final_mask] = self.target_network(non_final_next_states).max(1)[0].detach()
+        # Only compute Q-values for non-final states
+        next_state_values[non_final_next_states_mask] = self.target_network(non_final_next_states).max(1)[0].detach()
+        
         expected_state_action_values = (next_state_values * self.gamma) + reward_batch
+        
         loss = nn.functional.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
+        
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
+        
         self.epsilon = max(self.epsilon_min, self.epsilon_decay*self.epsilon)
+
 
     # Define a rolling average function
     def rolling_average(self, data, window_size):
